e2aa0d8175a26aa20ff3a28f41ac004420ad8e71
Georges Khaznadar <georgesk@debian.org>
2017-10-15 17:07:04 +0200

    ajout d'une traduction

---
+++ lang/fr.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="163"/>
+        <source>MainWindow</source>
+        <translation>Fenêtre principale</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="165"/>
+        <source>Start/Stop [Space bar]</source>
+        <translation>Démarrage/arrêt [Barre d&apos;espace]</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="166"/>
+        <source>Space</source>
+        <translation>Space</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="168"/>
+        <source>Rerun from beginning [Home]</source>
+        <translation>Relancer depuis le début [Début]</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="169"/>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="171"/>
+        <source>Launch simulation with the programs [F9]</source>
+        <translation>Lancer la simulation avec les programmes [F9]</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="172"/>
+        <source>F9</source>
+        <translation>F9</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="173"/>
+        <source>Operation</source>
+        <translation>Opération</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="174"/>
+        <source>Simulated scene</source>
+        <translation>Scène simulée</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="175"/>
+        <source>Save &amp;As</source>
+        <translation>Enregistrer so&amp;us</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="192"/>
+        <source>Check Synta&amp;x</source>
+        <translation>Vérifier la synta&amp;xe</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="177"/>
+        <source>Programs</source>
+        <translation>Programmes</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="178"/>
+        <source>Fi&amp;le</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="179"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="180"/>
+        <source>P&amp;rograms</source>
+        <translation>&amp;Programmes</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="181"/>
+        <source>&amp;Open Scene ...</source>
+        <translation>&amp;Ouvrir une scène ...</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="182"/>
+        <source>&amp;New Scene</source>
+        <translation>&amp;Nouvelle scène</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="183"/>
+        <source>Import S&amp;VG ...</source>
+        <translation>Importer S&amp;VG ...</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="184"/>
+        <source>&amp;Initial velocities</source>
+        <translation>Vitesses &amp;initiales</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="185"/>
+        <source>S&amp;cale</source>
+        <translation>É&amp;chelle</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="186"/>
+        <source>&amp;G ...</source>
+        <translation>&amp;G ...</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="187"/>
+        <source>&amp;About ...</source>
+        <translation>&amp;A propos ...</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="188"/>
+        <source>&amp;Help (F1)</source>
+        <translation>&amp;Aide (F1)</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="189"/>
+        <source>&amp;Quit (Ctrl-Q)</source>
+        <translation>&amp;Quitter (Ctrl-Q)</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="190"/>
+        <source>&amp;Save standalone</source>
+        <translation>Enregi&amp;strer tout seul</translation>
+    </message>
+    <message>
+        <location filename="../jongle/Ui_main.py" line="191"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="../jongle/main.py" line="135"/>
+        <source>video t = </source>
+        <translation>vidéo t= </translation>
+    </message>
+    <message>
+        <location filename="../jongle/main.py" line="166"/>
+        <source>The simulation is finished</source>
+        <translation>La simulation est finie</translation>
+    </message>
+</context>
+</TS>
