17ac80ee7228391deebee913c903b366e4cecba7
Georges Khaznadar <georgesk@debian.org>
2017-11-12 19:27:14 +0100

    valeurs initiales valides pour la vidéo

--- jongle/fonctions_base.py
+++ jongle/fonctions_base.py
@@ -1,13 +1,4 @@
 
-### placement initial de l'objet
-def initial(obj):
-    if obj.t==0:
-        obj.x=0
-        obj.y=0.7
-        obj.vx=0
-        obj.vy=0
-    return
-
 ### mise en place de la gravité
 def gravite(obj):
     obj.accelere(0,9.8)
@@ -15,6 +6,16 @@ def gravite(obj):
 
 ### mise en place du rebond
 def rebond(obj):
-    if obj.y > 1.5 and obj.vy > 0:
+    if obj.y > 1.8 and obj.vy > 0:
         obj.vy = -obj.vy
     return
+
+### placement initial de l'objet
+def initial(obj):
+    if obj.t==0:
+        obj.x=1.26
+        obj.y=1.64
+        obj.vx=-0.68
+        obj.vy=-2.4
+    return
+
