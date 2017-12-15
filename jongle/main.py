0e4288bdb3a0a57d0ee080029c9df6e10624cad3
Georges Khaznadar <georgesk@debian.org>
2017-12-15 16:36:05 +0100

    migration vers Python3 (et opencv version 3.2)

--- jongle/main.py
+++ jongle/main.py
@@ -1,6 +1,5 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
-from __future__ import print_function
 
 import sys, inspect, pydoc, os.path, cv2, io
 
--- versionData.py
+++ versionData.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
 
 from subprocess import Popen, PIPE
 import re
 
-project   = u'IREM-Jongle'
-copyright = u'2017, Georges Khaznadar'
-author    = u'Georges Khaznadar'
-version   = u'0.1'
-release   = u'0.1'
+project   = 'IREM-Jongle'
+copyright = '2017, Georges Khaznadar'
+author    = 'Georges Khaznadar'
+
+# version et release sont déduits des tags de Git
 
 p=Popen("git tag | grep -E 'v.*'", shell=True, stdout=PIPE)
 out, _ = p.communicate()
-release = unicode(max([version[1:] for version in out.strip().split("\n")]))
+release = max([version[1:] for version in out.strip().decode("utf-8").split("\n")])
 version = re.match(r"([.0-9]*).*", release).group(1)
 
 if __name__ == "__main__":
