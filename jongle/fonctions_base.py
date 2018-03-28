3e9dc577ab5714c364596808011bc2435076c2a9
Georges Khaznadar <georgesk@debian.org>
2018-03-28 17:00:03 +0200

    simplified the initial values

--- jongle/fonctions_base.py
+++ jongle/fonctions_base.py
@@ -12,17 +12,12 @@ def rebond(obj):
 
 ### placement initial de l'objet
 def initial(obj):
-    if obj.t==0 and obj.id=="ballon-1":
+    if obj.t==0 and obj.id=="ballon-0":
         # la vitesse initiale est (-0.57, -2.65) NB: l'axe y est vers le bas !
         obj.vx=-0.57
         obj.vy=-2.65
-        # la position initiale est (1.25, 1.38)
-        obj.x=1.25
-        obj.y=1.38
-        # mais ... on anticipe
-        # une étape de la simulation qui va modifier cette position
-        # pour un intervalle de temps de 0.04 secondes.
-        obj.x = obj.x - obj.vx*0.04
-        obj.y = obj.y - obj.vy*0.04
+        # la position initiale est (1.27, 1.49)
+        obj.x=1.27
+        obj.y=1.49
     return
 
